e91ddbbdbaf6fe247864971563d2062fcb545804
nazar-t <official.nazart@gmail.com>
2025-08-17 23:56:15 +0300

    promise fixed

--- src/app/[lang]/courses/page.tsx
+++ src/app/[lang]/courses/page.tsx
@@ -2,17 +2,20 @@ import { getDictionary } from '@/lib/getDictionary';
 import CoursesPageClient from './CoursesPageClient';
 
 interface CoursesPageProps {
-  params: {
+params: Promise<{
     lang: string;
-  };
+  }>;
 }
 
 const CoursesPage = async ({params}: CoursesPageProps) => {
-  const dictionary = await getDictionary(params.lang);
-
+  //const dictionary = await Promise.all(getDictionary(params.lang));
+  const { lang } = await params;
+  const [dict] = await Promise.all([
+    getDictionary(lang)
+  ]);
   return (
     <CoursesPageClient
-      dict={dictionary}
+      dict={dict}
     />
   );
 };
